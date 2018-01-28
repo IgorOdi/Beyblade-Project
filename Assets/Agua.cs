a27dcc3f491df47d72bce100ab84bce5648ec049
Felipemrmp <felipemrmp@gmail.com>
2018-01-28 18:28:11 -0200

    Teste Terreno

--- Assets/Agua.cs
+++ Assets/Agua.cs
@@ -13,4 +13,13 @@ public class Agua : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnTriggerStay2D(Collider2D hit)
+    {
+
+        if (hit.tag == "Beyblade") {
+
+            hit.gameObject.GetComponent<Beyblade>().actualStamina +=  50 * (int)Time.deltaTime;
+        }
+    }
 }
